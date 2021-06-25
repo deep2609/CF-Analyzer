6dfedc1c94aa0f4cab2c28fce06b429753f7724d
Souradip Mandal <67626543+deep2609@users.noreply.github.com>
2021-06-26 00:27:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,25 +8,13 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",function(req,res){
-  res.sendFile(__dirname + "/public/html/index.html");
+  res.sendFile(__dirname + "/public/html/data.html");
 });
 
 app.get("/data",function(req,res){
   res.sendFile(__dirname + "/public/html/data.html");
 });
 
-
-// app.post("/data",function(req,res){
-//     console.log(req.body.userName);
-//     const userName = req.body.userName;
-//     const url = "https://codeforces.com/api/user.info?handles="+userName;
-//
-//     request(url,function(error,response){
-//         let data = JSON.parse(response.body);
-//         console.log(data.result[0].problem);
-//     });
-//     res.send("done");
-// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
